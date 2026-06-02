5f14ee47e1a29a3d2f95157003f1295888c07416
Shubham Kumar <shubhsamkumar@192.168.0.114>
2026-06-02 16:22:25 +0530

    Updated project

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ document.body.style.backgroundColor =
 "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 };
-
 document.getElementById("fontSizeBtn").addEventListener("click", function () {
 
 fontSize += 2;
